2d3dd73461e6b3934c88084a0ee76a9bcc150a4a
Asharkhan468 <asharullah.khan2468@gmail.com>
2024-03-20 16:43:55 +0500

    todo app

--- app.js
+++ app.js
@@ -2,23 +2,37 @@ let todoInput=document.querySelector("#Todo");
 
 let ol=document.querySelector("ol");
 
-let array=[];
 
-function addTodo(){
+function renderarray(){
 
+    ol.innerHTML="";
 
-    array.push(todoInput.value);
-
-    console.log(array);
-
-    todoInput.value="";
 
 
 
     for(i=0 ; i<array.length ; i++){
-        ol.innerHTML+=`<li>${array[i]} <button onclick="deleteTodo()">Delete</button> <button>Edit</button></li>`;
+        ol.innerHTML+=`<li>${array[i]} <button onclick="deleteTodo(${i})">Delete</button> <button onclick="editTodo()>Edit</button></li>`;
     }
 
+    
+
+}
+
+let array=[];
+
+function addTodo(){
+
+    if(todoInput.value==""){
+        alert("Please Enter some task");
+    }
+
+
+    array.push(todoInput.value);
+
+    todoInput.value="";
+
+
+    renderarray()
     
 
  
@@ -26,11 +40,28 @@ function addTodo(){
 }
 
 
-function deleteTodo(){
+
+function deleteTodo(index){
+
+   
+    array.splice(index , 1);
+
+   renderarray()
+
+
+
+
+}
+
+
+function editTodo(){
+
+    let editedValue=prompt("Enter edited value");
+
+    array.push(editedValue);
+
+    renderarray()
 
     
-
-
-
 
 }
